51afa11506d03ecbb08c9f9a65a6b8ace97a13a2
David Alvarado <david.alvarado.barboza@gmail.com>
2024-05-31 11:33:07 -0600

    Progress with User's module

--- src/frontend/src/app/components/user/user.component.ts
+++ src/frontend/src/app/components/user/user.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Observable } from 'rxjs';
+import { User } from '../../models/user';
+import { AsyncPipe, CurrencyPipe, NgFor } from '@angular/common'
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, CurrencyPipe, NgFor],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+    Users!: Observable<User[]>;
+    public constructor(private _userService: UserService)
+    { }
 
+    ngOnInit(): void{
+      this.Users = this._userService.get("user");
+    }
 }
