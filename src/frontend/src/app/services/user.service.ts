9dd3dd1649fc90b55ee58ad648631a783d6902d5
David Alvarado <david.alvarado.barboza@gmail.com>
2024-05-31 23:50:17 -0600

    Restore user.service file

---
+++ src/frontend/src/app/services/user.service.ts
@@ -0,0 +1,33 @@
+import { HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { User } from "../models/user";
+import { Observable, map } from "rxjs";
+import { BaseService } from "./base.service";
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService extends BaseService {
+    getUsers(): Observable<User[]> {
+        return this.get("user");
+    }
+
+    login(user: User): Observable<any> {
+        let userJson = JSON.stringify(user);
+        let params = 'data=' + userJson
+        let headers = new HttpHeaders().set('Content-type', 'application/x-www-form-urlencoded')
+        let options = {
+            headers
+        }
+        return this._http.post(this.urlAPI + 'user/login', params, options)
+    }
+
+    getIdentityFromApi(token: string): Observable<any> {
+        let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+        headers = headers.set('Authorization', `Bearer ${token}`);
+
+        const options = { headers };
+        return this._http.get(this.urlAPI + 'user/identity', options);
+    }
+}
