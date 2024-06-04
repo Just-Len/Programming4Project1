70dfc6d2c04bfec92df96f9fc122a52f9cefe247
CocoCR300 <oscarrojasalvarado@hotmail.com>
2024-06-03 20:56:10 -0600

    Missing changes from previous commit
    * .-.

--- src/frontend/src/app/services/global.ts
+++ src/frontend/src/app/services/global.ts
@@ -1,4 +1,5 @@
 export const server = {
     url: 'http://localhost:8000/api/v1/',
-    storage: 'http://localhost:8000/storage/'
+    lodgingImages: 'http://localhost:8000/storage/lodgings/',
+    userImages: 'http://localhost:8000/storage/users/'
 }
