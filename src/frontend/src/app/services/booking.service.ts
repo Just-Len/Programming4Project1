619248bef4fcec7f916608d2007cf33d818285c4
Len <linakamado@gmail.com>
2024-06-05 22:27:31 -0600

    Routes update

--- src/frontend/src/app/services/booking.service.ts
+++ src/frontend/src/app/services/booking.service.ts
@@ -3,16 +3,23 @@ import { Observable } from "rxjs";
 import { BaseService } from "./base.service";
 import { AppResponse } from "../models/app_response";
 import { Booking } from "../models/booking";
+import { AppState } from "../models/app_state";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
     providedIn:'root'
 })
 export class BookingService extends BaseService
 {
+    constructor(protected appState: AppState, protected http: HttpClient) {
+        super(appState, http);
+      }
+    
+
     public postBooking(booking: Booking): Observable<AppResponse> {
         return this.post("booking", true, booking); 
     } 
-    public getBooking(booking: Booking): Observable<AppResponse>{
-        return this.get("booking", true);
-    }
+    public getBookingsByPersonId(personId: string): Observable<Booking[]> {
+        return this.get<Booking[]>(`/booking/${personId}`, true);
+      }
 }
